5d0e5420fecaffe75d7e6fe7160fbad66375e5bf
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2026-08-17 12:45:08 +0100

    add custom checks for ETT html

---
+++ docs/run_checks.rb
@@ -0,0 +1,9 @@
+# run custom checks defined in custom_html_tests.rb
+require 'html-proofer'
+
+require_relative 'tests/custom_html_tests.rb'
+
+# avoid h1 in Markdown - exclude spec pages which do have these (see README.md)
+HTMLProofer.check_directory("_site/", { checks: ["CheckH1NotInMarkdown"], ignore_files: [%r|_site/specification/.*|, "_site/index.html"]}).run
+# enforce h1 in front matter
+HTMLProofer.check_directory("_site/", { checks: ["CheckH1InIntro"], ignore_files: ["_site/index.html"]}).run 
